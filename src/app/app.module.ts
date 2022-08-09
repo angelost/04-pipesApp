38ed4bf139465ea3e8678fc2824032a2e156bbf9
Angelo <angelo.gonzalez@live.com>
2022-08-09 15:25:48 -0400

    140. Rutas de nuestra aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,    
-    SharedModule
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
