29c52aee6a5f0793c624d5ee9d0c5d63fc03fd00
Angelo <angelo.gonzalez@live.com>
2022-08-10 16:51:14 -0400

    146. Timezone y otros idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,10 @@ import { VentasModule } from './ventas/ventas.module';
 
 // Cambiar el locale de la app
 import localeEs from '@angular/common/locales/es-CL'
+import localeFr from '@angular/common/locales/fr'
 import { registerLocaleData } from '@angular/common';
 registerLocaleData( localeEs );
+registerLocaleData( localeFr );
 
 @NgModule({
   declarations: [
