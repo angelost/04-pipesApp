0332e15619ccb89c5aac292831aed7cf0bf8ef9c
Angelo <angelo.gonzalez@live.com>
2022-08-11 19:38:52 -0400

    156. AsyncPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -56,5 +57,15 @@ export class NoComunesComponent {
       vuela: false
     }
   ]
+
+  // Async Pipe
+  miObservable = interval(2000);
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('tenemos data de promesa')
+    }, 3500);
+  })
+
 
 }
